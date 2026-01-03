9a20c47da89cb8479b5813ef0529736700383500
Michael Schulz <michael.s.schulz@gmx.de>
2026-01-03 01:42:49 +0100

    MVP

--- src/TabelleAG-Grid.js
+++ src/TabelleAG-Grid.js
@@ -1,5 +1,5 @@
 import { AgGridReact } from 'ag-grid-react';
-import React, { useMemo, useState, StrictMode, useRef  } from "react";
+import React, { useMemo, useState, StrictMode, useRef, useEffect, useCallback } from "react";
 import 'ag-grid-community/styles/ag-grid.css';
 import 'ag-grid-community/styles/ag-theme-alpine.css';
 import { ModuleRegistry, AllCommunityModule, themeMaterial } from 'ag-grid-community';
@@ -13,7 +13,10 @@ const columns = ([
     { field: 'name' },
     { field: 'tag', rowGroup: true, hide: true },
     { field: 'villages' },
-    { field: 'points' }
+    { field: 'points' },
+    { field: 'iswinner' },
+    { field: 'willdelete' },
+    { field: 'willmaybe' }
 
 ]);
 
@@ -71,7 +74,10 @@ export async function loadAllyData() {
             ally_id,
             villages,
             points,
-            rank
+            rank,
+            iswinner,
+            willdelete,
+            willmaybe
         ] = playerline.split(',');
 
 
@@ -81,7 +87,10 @@ export async function loadAllyData() {
             ally_id: Number(ally_id),
             villages: Number(villages),
             points: Number(points),
-            rank: Number(rank)
+            rank: Number(rank),
+            iswinner: Boolean(iswinner),
+            willdelete: Boolean(willdelete),
+            willmaybe: Boolean(willmaybe),
         };
     });
     playerrows.sort((a, b) => a.rank - b.rank);
@@ -116,13 +125,61 @@ export async function loadAllyData() {
 
 const rows = await loadAllyData();
 
-const percentwithdeleting = rows.filter(row => row.name.includes('[LÖSCHEN]')).length / rows.length * 100;
-
-
+async function getNumberVillagesTotal() {
+    const data = rows;
+    const sumVillages = data.reduce((sum, player) => {
+        return sum + player.villages;
+    }, 0);
+    console.log(sumVillages);
+    return sumVillages;
+}
 
 
 
 export default function DataTable() {
+
+    const [rowData, setRowData] = useState([]);
+    const [sumVillagesWinner, setSumVillagesWinner] = useState(0);
+    const [sumVillagesSave, setSumVillagesSave] = useState(0);
+    const [sumVillagesMaybe, setSumVillagesMaybe] = useState(0);
+    
+
+    useEffect(() => {
+        async function calculateSumVillagesWinner() {
+            const sum = rowData.reduce((sum, player) => player.iswinner ? sum + player.villages : sum, 0);
+            setSumVillagesWinner(sum);
+        }
+        calculateSumVillagesWinner();
+    }, [rowData]);
+
+    useEffect(() => {
+        async function calculateSumVillagesSave() {
+            const sum = rowData.reduce((sum, player) => player.willdelete ? sum + player.villages : sum, 0);
+            setSumVillagesSave(sum);
+        }
+        calculateSumVillagesSave();
+    }, [rowData]);
+
+    useEffect(() => {
+        async function calculateSumVillagesMaybe() {
+            const sum = rowData.reduce((sum, player) => player.willmaybe ? sum + player.villages : sum, 0);
+            setSumVillagesMaybe(sum);
+        }
+        calculateSumVillagesMaybe();
+    }, [rowData]);
+
+    useEffect(() => {
+        async function loadData() {
+            const data = await loadAllyData(); // ⬅️ deine Funktion
+            setRowData(data);                  // ⬅️ HIER passiert es
+        }
+
+        loadData();
+    }, []);
+
+
+
+
     const [sum, setSum] = useState(0);
     const gridRef = useRef();
     const onSelectionChanged = () => {
@@ -136,6 +193,7 @@ export default function DataTable() {
 
         setSum(total);
     };
+
     const rowSelection = useMemo(() => {
         return {
             mode: 'multiRow',
@@ -143,8 +201,156 @@ export default function DataTable() {
             checkboxLocation: 'autoGroupColumn'
         };
     }, []);
+
+
     const containerStyle = useMemo(() => ({ width: "100%", height: "100%" }), []);
     const gridStyle = useMemo(() => ({ height: "100%", width: "100%" }), []);
+
+    const winner = () => {
+        const selectedNodes = gridRef.current.api.getSelectedNodes();
+
+        if (selectedNodes.length === 0) {
+            alert('Bitte eine Zeile auswählen');
+            return;
+        }
+
+        const idsToDelete = selectedNodes.map(n => n.data.id);
+
+        setRowData(prev =>
+            prev.map(row =>
+                idsToDelete.includes(row.id)
+                    ? { ...row, iswinner: true }
+                    : row
+            )
+        );
+
+
+
+    };
+
+    const notwinner = () => {
+        const selectedNodes = gridRef.current.api.getSelectedNodes();
+
+        if (selectedNodes.length === 0) {
+            alert('Bitte eine Zeile auswählen');
+            return;
+        }
+
+        const idsToDelete = selectedNodes.map(n => n.data.id);
+
+        setRowData(prev =>
+            prev.map(row =>
+                idsToDelete.includes(row.id)
+                    ? { ...row, iswinner: false }
+                    : row
+            )
+        );
+
+
+
+    };
+
+    const willDelte = () => {
+        const selectedNodes = gridRef.current.api.getSelectedNodes();
+
+        if (selectedNodes.length === 0) {
+            alert('Bitte eine Zeile auswählen');
+            return;
+        }
+
+        const idsToDelete = selectedNodes.map(n => n.data.id);
+
+        setRowData(prev =>
+            prev.map(row =>
+                idsToDelete.includes(row.id)
+                    ? { ...row, willdelete: true }
+                    : row
+            )
+        );
+
+
+
+    };
+    const willnotDelte = () => {
+        const selectedNodes = gridRef.current.api.getSelectedNodes();
+
+        if (selectedNodes.length === 0) {
+            alert('Bitte eine Zeile auswählen');
+            return;
+        }
+
+        const idsToDelete = selectedNodes.map(n => n.data.id);
+
+        setRowData(prev =>
+            prev.map(row =>
+                idsToDelete.includes(row.id)
+                    ? { ...row, willdelete: false }
+                    : row
+            )
+        );
+
+
+
+    };
+
+    const maybe = () => {
+        const selectedNodes = gridRef.current.api.getSelectedNodes();
+
+        if (selectedNodes.length === 0) {
+            alert('Bitte eine Zeile auswählen');
+            return;
+        }
+
+        const idsToDelete = selectedNodes.map(n => n.data.id);
+
+        setRowData(prev =>
+            prev.map(row =>
+                idsToDelete.includes(row.id)
+                    ? { ...row, willmaybe: true }
+                    : row
+            )
+        );
+
+
+
+    };
+
+    const notmaybe = () => {
+        const selectedNodes = gridRef.current.api.getSelectedNodes();
+
+        if (selectedNodes.length === 0) {
+            alert('Bitte eine Zeile auswählen');
+            return;
+        }
+
+        const idsToDelete = selectedNodes.map(n => n.data.id);
+
+        setRowData(prev =>
+            prev.map(row =>
+                idsToDelete.includes(row.id)
+                    ? { ...row, willmaybe: false }
+                    : row
+            )
+        );
+
+
+
+    };
+
+    const [villageCounttotal, setVillageCountTotal] = useState(0);
+
+    useEffect(() => {
+        async function loadVillageCountTotal() {
+            const number = await getNumberVillagesTotal();
+            setVillageCountTotal(number);
+        }
+
+        loadVillageCountTotal();
+    }, []);
+
+
+
+
 
     return (
 
@@ -154,12 +360,13 @@ export default function DataTable() {
                     <AgGridReact
                         ref={gridRef}
                         theme={themeMaterial}
-                        rowData={rows}
+                        rowData={rowData}
                         columnDefs={columns}
                         rowSelection={rowSelection}
                         pagination={true}
                         paginationPageSize={50}
                         onSelectionChanged={onSelectionChanged}
+                        getRowId={(params) => params.data.id}
 
 
                     />
@@ -168,6 +375,40 @@ export default function DataTable() {
             {/* Label */}
             <div style={{ marginTop: 10 }}>
                 <strong>Summe der selektierten Preise:</strong> {sum}
+            </div>
+
+            <div>
+                <div>
+                    <h1>Aktueller Stand</h1>
+                    <p>
+                        <label>Alle Dörfer: <strong>{villageCounttotal.toLocaleString()}</strong></label>
+
+                    </p>
+                    <p>
+                        <label>Siegerdörfer: <strong>{sumVillagesWinner}</strong></label>
+                        <label style={{ marginLeft: 10 }}>(x%)  </label>
+                    </p>
+
+
+                    <label>
+                        Safe Löschen Dörfer: <strong>{sumVillagesSave}</strong>
+                    </label>
+                    <br />
+                    <label>
+                        Vielleicht Löschen Dörfer: <strong>{sumVillagesMaybe}</strong>
+                    </label>
+
+                </div>
+                <div>
+                    <button onClick={winner}>Als Sieger Festlegen</button>
+                    <button onClick={willDelte}>Als Save Löschen Festlegen</button>
+                    <button onClick={maybe}>löscht vielleicht</button>
+                </div>
+                <div>
+                    <button onClick={notwinner}>Siegerplatz aberkennen</button>
+                    <button onClick={willnotDelte}>Save löschen entfernen</button>
+                    <button onClick={notmaybe}>vielleicht entfernen</button>
+                </div>
             </div>
         </div>
 
